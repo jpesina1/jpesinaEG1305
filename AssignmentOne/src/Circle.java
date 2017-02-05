291ba4cb3730b35f5856d1119c92bbc83400fcad
aktunc-STMU <aktunc@gmail.com>
2017-02-05 01:03:14 -0600

    Check comment

--- AssignmentOne/src/Circle.java
+++ AssignmentOne/src/Circle.java
@@ -5,6 +5,7 @@ public class Circle{
 		int radius = 3;
 		int diameter = 2*radius;
 		float area =(float)Math.PI *(radius*radius);
+		//Use double so you don't have to cast the result
 		
 		System.out.println("The circle's radius is "+radius);
 		System.out.println("The diameter is "+diameter);
